2a7eda28b149793fea93c75f0ef13ab288921ef2
Caitlyn Kim <cakim@smith.edu>
2025-10-16 19:19:49 -0400

    working on phase 3 code

--- CalculateInfix.java
+++ CalculateInfix.java
@@ -1,9 +1,36 @@
-import java.util.ArrayDeque;
+import java.util.ArrayDeque; //is this possible?
 
-public class CalculateInfix {//argument should be Queue?
-    public static Double infixToPostfix(ArrayDeque<Object> tokens) {
-        Stack<Character> stack = new Stack<>();
+public class CalculateInfix {
+    public static Double infixToPostfix(Queue<Object> tokens) {
+        Stack<Character> operatorStack = new Stack<>();
+        Queue<Object> outputQueue = new Queue<>();
 
+        while (!tokens.isEmpty()) {
+            Object token = tokens.remove();
+            
+            if (token instanceof Double) {
+                Double num = (Double) token;
+                outputQueue.add(num);
+            } else if (token instanceof Character) {
+                Character topStackOperator = operatorStack.getTail();
+                Character queueOperator = (Character) token;
+                while (topStackOperator instanceof Character &  precedence(topStackOperator) >= precedence(queueOperator)) {
+                    Character topOperator = operatorStack.pop();
+                    outputQueue.add(topOperator);
+                }
+            }
+        } 
         //use pemdas, parenthesis don't affect order of tokens
+    }
+
+    public static int precedence(Character token) {
+        //1 means highest precedence and 3 means lowest precedence
+        if (token == '^') {
+            return 1;
+        } else if (token == '*' | token == '/') {
+            return 2;
+        } else if (token == '+' | token == '-') {
+            return 3;
+        } return 0; //if operator isn't one of the above options return 0
     }
 }
--- CalculatePostfix.java
+++ CalculatePostfix.java
@@ -3,37 +3,49 @@ public class CalculatePostfix {
         Stack<Double> stack = new Stack<>();
 
         while (!tokens.isEmpty()) {
+            
             Object token = tokens.remove();
+
             if (token instanceof Double) {
                 Double num = (Double) token;
                 stack.push(num);
             } else if (token instanceof Character) {
                 Character operator = (Character) token;
                 
-                if (stack.size() < 2) {
+                if (stack.isEmpty()) {
                     throw new IllegalArgumentException("Not enough numbers in the stack to process operator. Entered expression was malformed.");
                 }
 
                 Double secondOperand = stack.pop();
-                Double firstOperand = stack.pop();
-                Double result;
-
-                if (operator == "+") {
-                    result = firstOperand + secondOperand;
-                } else if (operator == "-") {
-                    result = firstOperand - secondOperand;
-                } else if (operator == "*") {
-                    result = firstOperand * secondOperand;
-                } else if (operator == "/") {
-                    result = firstOperand / secondOperand;
-                } else if (operator == "^") {
-                    result = (int)pow(firstOperand, secondOperand);
+                
+                if (stack.isEmpty()) {
+                    throw new IllegalArgumentException("Not enough numbers in the stack to process operator. Entered expression was malformed.");
                 }
                 
-                stack.push(result);
-                System.out.println(stack);
+                Double firstOperand = stack.pop();
+
+                if (operator == '+') {
+                    stack.push(firstOperand + secondOperand);
+                } else if (operator == '-') {
+                    stack.push(firstOperand - secondOperand);
+                } else if (operator == '*') {
+                    stack.push(firstOperand * secondOperand);
+                } else if (operator == '/') {
+                    stack.push(firstOperand / secondOperand);
+                } else if (operator == '^') {
+                    stack.push(Math.pow(firstOperand, secondOperand));
+                } else {
+                    throw new IllegalArgumentException("Unknown operator: " + operator);
+                }
+
             }
         }
-        
+        Double result = stack.pop();
+
+        if (!stack.isEmpty()) {
+            throw new IllegalArgumentException("Too many items left in the stack. Entered expression was malformed.");
+        }
+
+        return result;
     }
 }
