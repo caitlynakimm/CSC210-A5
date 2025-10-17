4677cf4e55ed96368f938be21edffa0cfa75c939
Caitlyn Kim <cakim@smith.edu>
2025-10-16 21:44:26 -0400

    implemented methods for phase 3

--- CalculateInfix.java
+++ CalculateInfix.java
@@ -13,31 +13,59 @@ public class CalculateInfix {
                 outputQueue.add(num);
             } else if (token instanceof Character) {
                 Character c = (Character) token;
-                Character queueOperator = (Character) token;
-                while (!operatorStack.isEmpty() && operatorStack.peek() != '(' && precedence(operatorStack.peek()) >= precedence(queueOperator)) {
-                    Character topStackOperator = operatorStack.pop();
-                    outputQueue.add(topStackOperator);
-                }
-                operatorStack.push(queueOperator);
 
                 if (c == '(') {
                     operatorStack.push(c);
                 } else if (c == ')') {
-                    
+                    while(!operatorStack.isEmpty() && operatorStack.peek() != '(') {
+                        outputQueue.add(operatorStack.pop());
+
+                        if (operatorStack.isEmpty()) {
+                            throw new IllegalArgumentException("Couldn't find left parenthesis. There are mismatched parentheses.");
+                        }
+                    }
+                    operatorStack.pop(); //remove left parenthesis
+                } else {
+                    Character queueOperator = (Character) token;
+
+                    while (!operatorStack.isEmpty() && operatorStack.peek() != '(' && precedence(operatorStack.peek()) >= precedence(queueOperator)) {
+                        Character topStackOperator = operatorStack.pop();
+                        outputQueue.add(topStackOperator);
+                    }
+                    operatorStack.push(queueOperator);
+
                 }
             }
         } 
-        //use pemdas, parenthesis don't affect order of tokens
+
+        while (!operatorStack.isEmpty()) {
+            if (operatorStack.peek() == '(' | operatorStack.peek() == ')') {
+                throw new IllegalArgumentException("No more tokens to read and token at the top of the stack is a parenthesis. There are mismatched parentheses.");
+            } else {
+                outputQueue.add(operatorStack.pop());
+            }
+        }
+
+        
+        return CalculatePostfix.postfixToResult(outputQueue);
     }
 
     public static int precedence(Character token) {
         //1 means highest precedence and 3 means lowest precedence
         if (token == '^') {
-            return 1;
+            return 3;
         } else if (token == '*' | token == '/') {
             return 2;
         } else if (token == '+' | token == '-') {
-            return 3;
-        } return 0; //if operator isn't one of the above options return 0
+            return 1;
+        } return 0; //if operator isn't one of the above options (a parenthesis or unknown), return 0
+    }
+
+     public static void main(String[] args) {
+        //testing infix to postfix conversion by calling all three functions
+        String expression = "(5+2)/3";
+        Queue<Object> tokens = Tokenizer.readTokens(expression);
+        Double result = CalculateInfix.infixToPostfix(tokens);
+        System.out.println(expression + " = " + result);
     }
 }
